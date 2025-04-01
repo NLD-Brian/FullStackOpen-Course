7fadbc4b49ec53c5ac1f98c27a3d39a396f15b88
Blue-Wave Development <32848247+NLD-Brian@users.noreply.github.com>
2025-04-01 17:33:29 +0200

    implement notes fetching from json-server and update App component

--- part2/rendering-collections/src/App.jsx
+++ part2/rendering-collections/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState(
-    "A new note from the state of the component"
-  );
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
