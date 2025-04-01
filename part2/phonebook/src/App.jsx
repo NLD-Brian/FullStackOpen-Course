7802ab25eed6547781442662dd8fa8aba4dbbd79
Brian <32848247+NLD-Brian@users.noreply.github.com>
2025-04-01 22:05:41 +0200

    Add notification component and styling; implement phonebook service with CRUD operations

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import phonebookService from "./services/phonebook";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import PersonList from "./components/PersonList";
+import Notification from "./components/Notification";
+import "./index.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+  const [notification, setNotification] = useState(null);
+
+  useEffect(() => {
+    phonebookService
+      .getAll()
+      .then((initialPersons) => {
+        setPersons(initialPersons);
+      })
+  }, []);
 
   const personsExist = persons.some((person) => person.name === newName);
 
   const addContact = (event) => {
     event.preventDefault();
     if (personsExist) {
-      alert(`${newName} is already added to phonebook`);
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const personToUpdate = persons.find((person) => person.name === newName);
+        const updatedPerson = { ...personToUpdate, number: newNumber };
+        phonebookService.update(personToUpdate.id, updatedPerson).then((returnedPerson) => {
+          setPersons(persons.map((person) => (person.id !== personToUpdate.id ? person : returnedPerson)));
+          setNewName("");
+          setNewNumber("");
+          setNotification(`Updated ${returnedPerson.name}`);
+          setTimeout(() => {
+            setNotification(null);
+          }, 2500);
+        })
+        .catch(error => {
+          setNotification(`${personToUpdate.name} was already deleted from server`);
+          setTimeout(() => {
+            setNotification(null);
+          }, 2500);
+          setPersons(persons.filter((person) => person.id !== personToUpdate.id));
+      });
+      }
       return;
     }
     const nameObject = {
@@ -30,6 +56,21 @@ const App = () => {
     setPersons(persons.concat(nameObject));
     setNewName("");
     setNewNumber("");
+
+    phonebookService
+      .create(nameObject)
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName("");
+        setNewNumber("");
+        setNotification(`Added ${returnedPerson.name}`);
+          setTimeout(() => {
+            setNotification(null);
+          }, 2500);
+      })
+      .catch((error) => {
+        console.error("Error adding contact:", error);
+      });
   };
 
   const handleSearchChange = (event) => {
@@ -49,6 +90,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} />
       <Filter search={search} handleSearchChange={handleSearchChange} />
 
       <h3>Add a new</h3>
--- part2/phonebook/src/components/PersonList.jsx
+++ part2/phonebook/src/components/PersonList.jsx
@@ -1,9 +1,18 @@
+import phonebookService from "../services/phonebook";
 const PersonList = ({ persons }) => {
-  return (
+  const handleDelete = (person) =>
+  {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      phonebookService.remove(person.id)
+    }
+  };
+
+return (
     <ul>
       {persons.map((person) => (
         <li key={person.id}>
           {person.name} - {person.number}
+          <button onClick={() => handleDelete(person)}>Delete</button>
         </li>
       ))}
     </ul>
