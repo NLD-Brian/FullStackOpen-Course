8758b2c4a077a5faebc39e3508b4e170cbcb21bc
Brian <32848247+NLD-Brian@users.noreply.github.com>
2025-04-03 13:45:48 +0200

    Add .gitignore files for phonebook and nodejs-server; update package.json for dependencies and scripts

--- part3/nodejs-server/index.js
+++ part3/nodejs-server/index.js
@@ -1,8 +1,79 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World\n');
+let notes = [
+    {
+      id: "1",
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: "2",
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: "3",
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+  const requestLogger = (req, res, next) => {
+    console.log('Method:', req.method)
+    console.log('Path:', req.path)
+    console.log('Body:', req.body)
+    console.log('---')
+    next()
+}
+app.use(express.json())
+app.use(requestLogger)
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+
+// Middleware to log the request method and URL
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+        res.json(note)
+    }
+    else {
+        res.status(404).send("Note ID could not be found.").end()
+    }
+})
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    notes = notes.filter(note => note.id !== id)
+    res.status(204).end()
+})
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+    if (!body.content) {
+        return res.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+    res.json(note)
 })
 
 const PORT = 3001;
---
+++ part3/phonebook/index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express()
+const morgan = require('morgan')
+
+app.use(express.json())
+// Create a custom token for logging request body
+morgan.token('req-body', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : '-'
+  });
+  
+  // Configure morgan with custom format
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+
+let persons = 
+    [
+        { 
+          "id": "1",
+          "name": "Arto Hellas", 
+          "number": "040-123456"
+        },
+        { 
+          "id": "2",
+          "name": "Ada Lovelace", 
+          "number": "39-44-5323523"
+        },
+        { 
+          "id": "3",
+          "name": "Dan Abramov", 
+          "number": "12-43-234345"
+        },
+        { 
+          "id": "4",
+          "name": "Mary Poppendieck", 
+          "number": "39-23-6423122"
+        }
+    ]
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.random(...persons.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).send("Person ID could not be found.").end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if (!body.name || !body.number) {
+        return res.status(400).json({ 
+            error: 'name or number missing' 
+        })
+    }
+
+    const personExists = persons.some(person => person.name === body.name)
+    if (personExists) {
+        return res.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
+
+    const newPerson = {
+        id: String(persons.length + 1),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(newPerson)
+    res.json(newPerson)
+    morgan('tiny')
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`Phonebook has info for ${persons.length} people <br> ${date}`)
+})
+
+
+const PORT = 3001;
+app.listen(PORT)
+console.log(`Server running at http://localhost:${PORT}/`);
