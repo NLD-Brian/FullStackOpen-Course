53c4c7f7be507b2077e5f693bf81d574e5c2d3d1
Brian <32848247+NLD-Brian@users.noreply.github.com>
2025-04-05 22:02:57 +0200

    Update .gitignore files, add dotenv and mongoose dependencies, and implement MongoDB connection in models

--- part3/nodejs-server/index.js
+++ part3/nodejs-server/index.js
@@ -1,84 +1,112 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+const Note = require('./Models/note')
 const app = express()
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-  const requestLogger = (req, res, next) => {
-    console.log('Method:', req.method)
-    console.log('Path:', req.path)
-    console.log('Body:', req.body)
-    console.log('---')
-    next()
+
+// let notes = [
+//     {
+//       id: "1",
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: "2",
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: "3",
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:', req.path)
+  console.log('Body:', req.body)
+  console.log('---')
+  next()
 }
 app.use(express.json())
-app.use(cors())
 app.use(requestLogger)
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-  }
+app.use(express.static('dist'))
 
 // Middleware to log the request method and URL
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
     res.json(notes)
+  })
 })
 app.get('/api/notes/:id', (req, res) => {
-    const id = req.params.id
-    const note = notes.find(note => note.id === id)
-    
+  Note.findById(req.params.id).then(note => {
     if (note) {
-        res.json(note)
+      res.json(note)
     }
     else {
-        res.status(404).send("Note ID could not be found.").end()
+      res.status(404).send("Note ID could not be found.").end()
     }
+  })
+    .catch(error => next(error))
 })
 app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
+  Note.findByIdAndDelete(req.params.id)
+    .then(res => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 app.post('/api/notes', (req, res) => {
-    const body = req.body
-    if (!body.content) {
-        return res.status(400).json({ 
-            error: 'content missing' 
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId()
-    }
-
-    notes = notes.concat(note)
-    res.json(note)
+  const body = req.body
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001;
+app.put('/api/notes/:id', (req, res, next) => {
+  const { content, important } = req.body
+
+  Note.findById(req.params.id)
+    .then(note => {
+      if (!note) {
+        return res.status(404).send("Note ID could not be found.").end()
+      }
+      note.content = content
+      note.important = important
+
+      return note.save().then(updatedNote => {
+        res.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
