5f3ea8a85368473b907b62a2b193e8dbfab08881
Brian <32848247+NLD-Brian@users.noreply.github.com>
2025-04-06 22:31:34 +0200

    feat: initialize bloglistapp with package.json and basic dependencies
    feat: set up notesapp with Express, MongoDB connection, and middleware
    
    feat: implement CRUD operations for notes in notesapp
    
    test: add unit tests for average and reverse utility functions
    
    feat: create configuration and utility functions for testing
    
    feat: implement logging functionality for notesapp
    
    feat: add middleware for request logging and error handling

---
+++ part4/bloglistapp/Controllers/blogs.js
@@ -0,0 +1,38 @@
+const bloglistRouter = require('express').Router()
+const Blog = require('../Models/blog')
+
+bloglistRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+bloglistRouter.get('/:id', (request, response, next) => {
+    Blog.findById(request.params.id).then(blog => {
+        if (blog) {
+            response.json(blog)
+        } else {
+            response.status(404).end()
+        }
+    })
+        .catch(error => next(error))
+})
+
+bloglistRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0
+    })
+
+    blog.save().then(savedBlog => {
+        response.json(savedBlog)
+    })
+        .catch(error => next(error))
+})
+
+module.exports = bloglistRouter
+
