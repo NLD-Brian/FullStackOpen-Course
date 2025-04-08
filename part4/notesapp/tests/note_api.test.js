38450dac901daca8481265de9229b8f80f773689
Brian <32848247+NLD-Brian@users.noreply.github.com>
2025-04-08 21:52:28 +0200

    part 4.5

--- part4/notesapp/tests/note_api.test.js
+++ part4/notesapp/tests/note_api.test.js
@@ -11,11 +11,10 @@ const Note = require('../models/note')
 beforeEach(async () => {
     await Note.deleteMany({})
 
-    let noteObject = new Note(helper.initialNotes[0])
-    await noteObject.save()
-
-    noteObject = new Note(helper.initialNotes[1])
-    await noteObject.save()
+    const noteObjects = helper.initialNotes
+        .map(note => new Note(note))
+    const promiseArray = noteObjects.map(note => note.save())
+    await Promise.all(promiseArray)
 })
 
 test('notes are returned as json', async () => {
@@ -71,6 +70,34 @@ test('note without content is not added', async () => {
     const notesAtEnd = await helper.notesInDb()
 
     assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToView = notesAtStart[0]
+
+    const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    assert.deepStrictEqual(resultNote.body, noteToView)
+})
+
+test('a note can be deleted', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToDelete = notesAtStart[0]
+
+    await api
+        .delete(`/api/notes/${noteToDelete.id}`)
+        .expect(204)
+
+    const notesAtEnd = await helper.notesInDb()
+
+    const contents = notesAtEnd.map(r => r.content)
+    assert(!contents.includes(noteToDelete.content))
+
+    assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
 })
 
 after(async () => {
