38df4de260b59786f583eef92b19ba43319a5677
Brian <32848247+NLD-Brian@users.noreply.github.com>
2025-04-19 20:15:43 +0200

    exercise 5.2

--- part5/notesapp/backend/app.js
+++ part5/notesapp/backend/app.js
@@ -1,22 +1,20 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
-
-const bloglistRouter = require('./Controllers/blogs')
-const usersRouter = require('./Controllers/users')
-const loginRouter = require('./Controllers/login')
+const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose
-    .connect(config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
     .then(() => {
         logger.info('connected to MongoDB')
     })
@@ -24,13 +22,12 @@ mongoose
         logger.error('error connection to MongoDB:', error.message)
     })
 
-app.use(cors())
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
 
-app.use('/api/blogs', middleware.userExtractor, bloglistRouter)
+app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
